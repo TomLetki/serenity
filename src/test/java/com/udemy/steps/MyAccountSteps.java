38a88be6fd30184dcb5422f0bfa756bcf5c9689f
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2022-04-13 09:53:06 +0200

    formatted: MyAccountSteps

--- src/test/java/com/udemy/steps/MyAccountSteps.java
+++ src/test/java/com/udemy/steps/MyAccountSteps.java
@@ -7,18 +7,19 @@ import net.thucydides.core.steps.ScenarioSteps;
 public class MyAccountSteps {
 
     private MyAccountPage myAccountPage;
-   @Step("Register user with email {0} and password {1}")
-    public void registerUser(String email, String password){
+
+    @Step("Register user with email {0} and password {1}")
+    public void registerUser(String email, String password) {
         myAccountPage.registerUser(email, password);
     }
 
     @Step("Checking if logout link is displayed")
-    public boolean checkLogoutLink(){
-      return myAccountPage.isLogoutLinkDisplayed();
+    public boolean checkLogoutLink() {
+        return myAccountPage.isLogoutLinkDisplayed();
     }
 
     @Step("Checking if error message equals {0}")
-    public boolean checkErorMessage(String message){
-       return myAccountPage.checkErrorMsg(message);
+    public boolean checkErorMessage(String message) {
+        return myAccountPage.checkErrorMsg(message);
     }
 }
